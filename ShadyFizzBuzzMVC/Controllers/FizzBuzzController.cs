a974839a2d32ad940835dd782da7adaaab6d241d
Andrew Russell <67603030+ARussell-CF@users.noreply.github.com>
2021-10-12 15:01:33 -0400

    Initial commit

---
+++ ShadyFizzBuzzMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShadyFizzBuzzMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShadyFizzBuzzMVC.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        [HttpGet]
+        public IActionResult FizzBuzz()
+        {
+            var model = new FizzBuzzViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult FizzBuzz(FizzBuzzViewModel model)
+        {
+
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % model.FizzValue == 0 && i % model.BuzzValue == 0)
+                {
+                    //returnArray.push('FizzBuzz');
+                    model.Results.Add("FizzBuzz");
+                }
+                else if (i % model.FizzValue == 0)
+                {
+                    model.Results.Add("Fizz");
+
+                }
+                else if (i % model.BuzzValue == 0)
+                {
+                    model.Results.Add("Buzz");
+                }
+                else
+                {
+                    model.Results.Add(i.ToString());
+                }
+            }
+
+            var test = model.Results.Count;
+
+            return View(model);
+        }
+    }
+}
---
+++ ShadyFizzBuzzMVC/Models/FizzBuzzViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShadyFizzBuzzMVC.Models
+{
+    public class FizzBuzzViewModel
+    {
+        //I need:
+        //FizzValue
+        public int FizzValue { get; set; } = 3;
+
+        //BuzzValue
+        public int BuzzValue { get; set; }
+
+        //A way to output my results
+        public List<string> Results { get; set; } = new List<string>();
+
+    }
+}
